2e3c5222b6fff2e989d798b1a373af73398bfe58
luisaromero <luisa.marcela.romero@gmail.com>
2026-06-22 21:07:40 -0400

    get data from all users

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,2 +1,19 @@
 const URL = "https://jsonplaceholder.typicode.com/users"
 
+const getUsersButton = document.getElementById('btn-get-users')
+
+getUsersButton.addEventListener('click', () => {
+    fetch(URL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No se pudo conectar')
+            }
+            return response.json()
+        })
+
+        .then(data => {
+            charactersData = data;
+            console.log({ data })
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
