98ed7ab24b7c40af775b7ae008cd6c010f897697
luisaromero <luisa.marcela.romero@gmail.com>
2026-06-22 22:03:47 -0400

    add get user by name

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,19 +1,74 @@
 const URL = "https://jsonplaceholder.typicode.com/users"
 
 const getUsersButton = document.getElementById('btn-get-users')
+const searchByName = document.getElementById('btn-search-user')
 
-getUsersButton.addEventListener('click', () => {
-    fetch(URL)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('No se pudo conectar')
-            }
-            return response.json()
-        })
 
-        .then(data => {
-            charactersData = data;
-            console.log({ data })
-        })
-        .catch(error => console.error('Error fetching data:', error));
+
+class getUserData {
+    constructor() {
+        this.url = URL;
+        this.users = [];
+        this.getData();
+    }
+
+    getData() {
+        fetch(URL)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('No se pudo conectar')
+                }
+                return response.json()
+            })
+
+            .then(data => {
+                this.users = data
+                console.log({ data })
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+    }
+
+    getAllnames() {
+        this.users.forEach(user => {
+            console.log(user.name);
+        });
+    }
+
+    getuserByName(name) {
+        if (name.length > 0) {
+            return this.users.find(
+                user => user.name.toLowerCase() === name.toLowerCase()
+            );
+        }
+
+        else {
+            console.error('Ingrese un nombre de usuario válido')
+        }
+    }
+
+    showUsernameAndEmail() {
+        const promptSearchByname = prompt("Ingrese el nombre del usuario:");
+        const nameUser = promptSearchByname.trim()
+
+        const userFound = this.getuserByName(nameUser);
+
+        if (userFound) {
+            console.log("Username:", userFound.username);
+            console.log("Correo:", userFound.email);
+        } else {
+            console.log("Usuario no encontrado");
+        }
+    }
+
+
+}
+
+const _users = new getUserData();
+
+getUsersButton.addEventListener("click", () => {
+    _users.getData();
 });
+searchByName.addEventListener('click', () => {
+    _users.showUsernameAndEmail();
+})
